70ec2a67f7270184e79ce9e5054b599d344b567b
Alonso <jalonsomendoza32@gmail.com>
2019-12-24 10:59:31 -0600

    Created basic getters and setters for basic variables of PWMChanger

--- PWM_Changer.cpp
+++ PWM_Changer.cpp
@@ -1,3 +1,52 @@
-#include "PWM_Changer.h"
+#include "./PWM_Changer.h"
 
-PWMChanger::PWMChanger() {}
+PWMChanger::PWMChanger() 
+{
+    
+}
+
+/**
+ * Getters for the the variables used in this class
+**/
+int PWMChanger::getMinPWM()
+{
+    return this->minPWM;
+}
+
+int PWMChanger::getMaxPWM()
+{
+    return this->maxPWM;
+}
+
+float PWMChanger::getCurrentPWM()
+{
+    return this->currentPWM;
+}
+
+int PWMChanger::getTarget()
+{
+    return this->target;
+}
+
+/**
+ * Setters for the variables used in this class
+**/
+void PWMChanger::setMinPWM(int minPWM)
+{
+    this->minPWM = minPWM;
+}
+
+void PWMChanger::setMaxPWM(int maxPWM)
+{
+    this->maxPWM = maxPWM;
+}
+
+void PWMChanger::setCurrentPWM(float currentPWM)
+{
+    this->currentPWM = currentPWM;
+}
+
+void PWMChanger::setTarget(int target)
+{
+    this->target = target;
+}
--- PWM_Changer.h
+++ PWM_Changer.h
@@ -2,9 +2,25 @@
 #define PWM_Changer_h
 
 
-class PWMChanger {
+class PWMChanger 
+{
+private:
+    // variables
+    int minPWM = 0;
+    int maxPWM = 0;
+    float currentPWM = 0.0;
+    int target = 0;
 public:
     PWMChanger(); // Constructor
+    // getters and setters for the variables
+    int getMinPWM();
+    int getMaxPWM();
+    float getCurrentPWM();
+    int getTarget();
+    void setMinPWM(int);
+    void setMaxPWM(int);
+    void setCurrentPWM(float);
+    void setTarget(int);
 };
 
 
